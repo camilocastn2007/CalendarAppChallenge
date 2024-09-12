5fb28de400d2010cced9012a9269747a32c64fb2
camilo <Brayancamilogt@gmail.com>
2024-09-12 11:37:26 -0500

    Class Event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -20,4 +20,31 @@ class Reminder:
         f"Reminder on {self.date_time} of type {self.type}"
 
 
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder] = field(default=list)
+    id: str = field(default_factory=generate_unique_id())
+
+    def add_reminder(self,date_time :datetime,type :str):
+
+        reminder = Reminder(date_time=datetime,type=type)
+        self.reminders = Reminder.append(reminder)
+
+    def delete_reminder(self,reminder_ex :int):
+        pass
+
+    def __str__(self):
+        return
+        ID: {self.id}
+        Event_title: {self.title}
+        Description: {self.description}
+        Time: {self.start_at} - {self.end_at}
+
+
+
 
